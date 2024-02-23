eb8e561ed5387b73cfb1880ef0c894aedd9b941b
EkaterinaTokar <ekaterinatokar19@gmail.com>
2024-02-23 14:21:04 +0500

    feat: implemented websocket server

---
+++ src/websocket/wsserver.ts
@@ -0,0 +1,34 @@
+import { WebSocketServer, WebSocket } from 'ws';
+import {} from "./interface.js"
+
+
+export const wsServer = new WebSocketServer({ port: 3000 });
+wsServer.on('connection',  (ws: WebSocket) => {
+    console.log('connect');
+    ws.on('message', (message: string) => {
+        const data = JSON.parse(message);
+        switch (data.type) {
+            case 'reg':
+                console.log('reg:', data.type);
+                break;
+            case 'create_room':
+                console.log('create_room:', data.type);
+                break;
+            case 'add_user_to_room':
+                console.log('add_user_to_room:', data.type);
+                break;
+            case 'add_ships':
+                console.log('add_ships:', data.type);
+            break;
+           case 'attack':
+           case 'randomAttack':
+                console.log('attack:', data.type);
+            break;
+            default:
+                console.log('Unknown message type:', data.type);
+        }
+    });
+    ws.on('close', () => {
+        console.log('close connection');
+    });
+});
